b84a60ef0f794b430bcf6a3b503cf53bb0536cef
Hugo Salles <salles.hugo05@gmail.com>
2022-08-08 14:24:53 -0400

    creating new props and editing them

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import { useState } from 'react';
-import Menubar from './components/Menubar';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import './App.css';
+import { useState } from "react";
+import Menubar from "./components/Menubar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-  const [name, setName] = useState('Hugo')
+  const [name, setName] = useState("Hugo");
   return (
     <>
-    <Menubar/>
-    <Main 
-    name={name}
-    fruit="Banana"
-    total={23}
-    isAmazing={true} />
-    <Footer/>
-    
-    
+      <Menubar />
+      <Main name={name} />
+      <Main name="Mateo" />
+      <Main name="Pam" />
+      <Main name="Derek" />
+      <Footer />
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,7 @@
-
-
-export default function Main (props) {
-    const {name, total, isAwesome, fruit} = props;
-    return (
-        <main>
-        <h1>Hello {name}</h1>
-
-        </main>
-    )
+export default function Main({ name }) {
+  return (
+    <main>
+      <h1>Hello {name}</h1>
+    </main>
+  );
 }
